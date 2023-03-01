b895d735b195774524b90af67553a474fb893904
hebercardonap <heber.cardona@polaris.com>
2023-03-01 13:46:45 -0600

    updated test

--- tests/BuildSubmission.spec.ts
+++ tests/BuildSubmission.spec.ts
@@ -284,7 +284,7 @@ test(`@smoke Verify atv debug en-us stepped process and build submission`, async
       await pages.build.performFeatureDefaultSelections();
     });
     await test.step(`Click any available furniture layout`, async () => {
-      await pages.build.clickAvailableLayoutItem();
+      await pages.build.clickGdyHurAvailableLayoutItem();
     });
     await test.step(`Click footer next button`, async () => {
       await pages.build.clickFooterNextBtn();
@@ -315,7 +315,7 @@ test(`@smoke Verify atv debug en-us stepped process and build submission`, async
         await pages.build.clickHurModelCategory('SunDeck OB');
       });
       await test.step('Click any available layout item', async () => {
-        await pages.build.clickAvailableLayoutItem();
+        await pages.build.clickGdyHurAvailableLayoutItem();
       });
       await test.step('Click footer button', async () => {
         await pages.build.clickFooterNextBtn(); 
@@ -329,31 +329,6 @@ test(`@smoke Verify atv debug en-us stepped process and build submission`, async
       });
     }); 
   }
-
-  test.only(`@regression @hur Verify hur build submission for debug`, async ( { pages } ) => {
-    await test.step(`Navigate to hurricane debug start build page`, async () => {
-      await pages.navigation.navigateToStartingBuildUrl(Brands.hur, 'en-ca');
-    });
-    await test.step('Click hurricane boat series category', async () => {
-      await pages.build.clickHurBoatSeries('SunDeck Series');
-    });
-    await test.step('Click hurricane model', async () => {
-      await pages.build.clickHurModelCategory('SunDeck OB');
-    });
-    await test.step('Click any available layout item', async () => {
-      await pages.build.clickAvailableLayoutItem();
-    });
-    await test.step('Click footer button', async () => {
-      await pages.build.clickFooterNextBtn(); 
-    });
-    await test.step(`Open build summary and click I am Finished`, async () => {
-      await pages.build.openSummaryHur();
-    await pages.build.clickIamFinishedBtn();
-    });
-    await test.step(`Fill quote form details and submit`, async () => {
-      await pages.quote.enterHurFormDetailsAndSubmit();
-    });
-  }); 
 
 
 
