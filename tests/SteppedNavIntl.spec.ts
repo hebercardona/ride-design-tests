26ec7264b94b54051dc6f1ecab4c7d7ca718787c
hebercardonap <heber.cardona@polaris.com>
2023-03-03 10:55:31 -0600

    updated tests

--- tests/SteppedNavIntl.spec.ts
+++ tests/SteppedNavIntl.spec.ts
@@ -6,49 +6,19 @@ import { BuildPage } from "@pages/BuildPage";
 
   for (const locale of testConfig.internationalLocales.rzr) {
     test(`Verify rzr intl stepped process ${locale} @intl`, async ( { pages } ) => {
-
-        let accessoryAdded;
         await test.step(`Navigate to intl start build page`, async () => {
             await pages.navigation.navigateToPreviousYearStartingBuildUrl('rzr', locale);
         });
-        await test.step(`Select any model and go to accessories page`, async () => {
-            await pages.build.modelSelectionToAccessoriesPage();
-        });
-        await test.step(`Verify carousel is displayed`, async () => {
-            expect(await pages.build.carousel.isCarouselPresent(), 'Carousel not displayed').toBeTruthy();
-        });
-        await test.step(`Open build summary and click I am Finished`, async () => {
-            await pages.build.openBuildSummaryAndClickImFinished();
-        });
-        await test.step(`Verify build quote response status`, async() => {
-            expect(await pages.page.waitForResponse(response => 
-                response.url().includes('quote') && response.status() === 200))
-                .toBeTruthy();
-        })
+        await testSteps(pages);
       }); 
   }
 
   for (const locale of testConfig.internationalLocales.rgr) {
     test(`Verify rgr intl stepped process ${locale} @intl`, async ( { pages } ) => {
-
-        let accessoryAdded;
         await test.step(`Navigate to intl start build page`, async () => {
             await pages.navigation.navigateToPreviousYearStartingBuildUrl('rgr', locale);
         });
-        await test.step(`Select any model and go to accessories page`, async () => {
-            await pages.build.modelSelectionToAccessoriesPage();
-        });
-        await test.step(`Verify carousel is displayed`, async () => {
-            expect(await pages.build.carousel.isCarouselPresent(), 'Carousel not displayed').toBeTruthy();
-        });
-        await test.step(`Open build summary and click I am Finished`, async () => {
-            await pages.build.openBuildSummaryAndClickImFinished();
-        });
-        await test.step(`Verify build quote response status`, async() => {
-            expect(await pages.page.waitForResponse(response => 
-                response.url().includes('quote') && response.status() === 200))
-                .toBeTruthy();
-        })
+        await testSteps(pages);
       }); 
   }
 
@@ -57,7 +27,7 @@ import { BuildPage } from "@pages/BuildPage";
         await test.step(`Navigate to intl start build page`, async () => {
             await pages.navigation.navigateToPreviousYearStartingBuildUrl('grl', locale);
         });
-        await steps(pages);
+        await testSteps(pages);
       }); 
   }
 
@@ -66,11 +36,11 @@ import { BuildPage } from "@pages/BuildPage";
         await test.step(`Navigate to intl start build page`, async () => {
             await pages.navigation.navigateToPreviousYearStartingBuildUrl('atv', locale);
         });
-        await steps(pages);
+        await testSteps(pages);
       }); 
   }
 
-  const steps = async (pages: pages) => {
+  const testSteps = async (pages: pages) => {
     await test.step(`Select any model and go to accessories page`, async () => {
         await pages.build.modelSelectionToAccessoriesPage();
     });
