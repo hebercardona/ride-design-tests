31e0285d82a9be429ff8355ef794d21920a609a3
hebercardonap <heber.cardona@polaris.com>
2023-03-06 13:01:02 -0600

    added ind test

--- tests/SteppedNavIntl.spec.ts
+++ tests/SteppedNavIntl.spec.ts
@@ -1,8 +1,6 @@
 import { testConfig } from "@testConfig";
 import { expect, test } from "@framework/BaseTest";
-import { Brands } from "@framework/Brands";
-import { BasePage, pages } from "@pages/BasePage";
-import { BuildPage } from "@pages/BuildPage";
+import { pages } from "@pages/BasePage";
 
   for (const locale of testConfig.internationalLocales.rzr) {
     test(`Verify rzr intl stepped process ${locale} @intl`, async ( { pages } ) => {
@@ -39,6 +37,28 @@ import { BuildPage } from "@pages/BuildPage";
         await testSteps(pages);
       }); 
   }
+
+  for (const locale of testConfig.internationalLocales.ind) {
+    test(`Verify indian intl stepped process ${locale} @intl`, async( { pages } ) => {
+        await test.step(`Navigate to intl ${locale} start build page`, async () => {
+            await pages.navigation.navigateToPreviousYearStartingBuildUrl('ind', locale);
+        });
+        await test.step(`Navigate to accessories page with any selection`, async () => {
+            await pages.build.categoryToAccessoriesPageInd();
+        });
+        await test.step(`Verify carousel is displayed`, async () => {
+            expect(await pages.build.carousel.isCarouselPresent(), 'Carousel not displayed').toBeTruthy();
+        });
+        await test.step(`Open build summary and click I am Finished`, async () => {
+            await pages.build.openBuildSummaryAndClickImFinished();
+        });
+        await test.step(`Verify build quote response status`, async() => {
+            expect(await pages.page.waitForResponse(response => 
+                response.url().includes('quote') && response.status() === 200))
+                .toBeTruthy();
+        });
+    });
+  };
 
   const testSteps = async (pages: pages) => {
     await test.step(`Select any model and go to accessories page`, async () => {
