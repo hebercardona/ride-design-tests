98d9f2525d8c33c19bf8bc0c37c9dead539a0ce1
Heber Cardona <HCardo@polarisind.com>
2023-03-22 16:50:50 -0500

    added teststep

--- tests/BuildLoad.spec.ts
+++ tests/BuildLoad.spec.ts
@@ -93,3 +93,34 @@ const orvBuildLoadTestSteps = async (pages: pages, testInfo: TestInfo) => {
         expect(comparator(beforeImg, afterImg)).toBeNull();
     });
 }
+
+const indBuildLoadTestSteps = async (pages: pages, testInfo: TestInfo) => {
+    let beforeImg;
+    let afterImg;
+    let loadUrl;
+
+    await test.step('Open build summary and take snapshot for further validation', async() => {
+        await pages.build.openSummary();
+        beforeImg = await  (await pages.build.summary.getBuildsummaryDialogElement()).screenshot({path: `screenshots/${testInfo.title}_before.png`});
+    });
+    await test.step('Click I am Finished to get to build quote page', async() => {
+        await pages.build.clickIamFinishedBtn();
+    });
+    await test.step('Get submissionID from url and get load url from database', async() => {
+        const buildId = Common.getBuildIdFromQuoteUrl(pages.page.url());
+        const loadUrlQuery = await SqlHelper.executeQuery(`select LoadUrl from ConfiguredWholegoods where BuildID = '${buildId}'`);
+        loadUrl = loadUrlQuery.recordset[0].LoadUrl;
+    });
+    await test.step('Navigate to load url and verify response is successful', async() => {
+        await pages.navigation.navigateToUrl(loadUrl);
+        await pages.build.modals.clickPurposePromptNewVehicle();
+        await pages.build.waitForPcLoaded();
+    });
+    await test.step('Open build summary and take snapshot after load url loaded', async() => {
+        await pages.build.openSummary();
+        afterImg = await  (await pages.build.summary.getBuildsummaryDialogElement()).screenshot({path: `screenshots/${testInfo.title}_after.png`})
+    });
+    await test.step('Compare build summary snapshots', async() => {
+        expect(comparator(beforeImg, afterImg)).toBeNull();
+    });
+}
