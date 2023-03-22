d43069003af0c7f72d3e0daadd063af329eef19b
hebercardonap <43042222+hebercardonap@users.noreply.github.com>
2023-03-22 15:26:19 -0500

    added tests

--- tests/BuildLoad.spec.ts
+++ tests/BuildLoad.spec.ts
@@ -12,8 +12,122 @@ for (const locale of testConfig.domesticLocales.rzr) {
         let afterImg;
         let loadUrl;
     
-        await test.step('Navigate to any build url', async() => {
+        await test.step('Navigate to any rzr build url', async() => {
             const url = await ApiData.getApiBuildUrl(locale, 'rzr');
+            await pages.navigation.navigateToUrl(url);
+            await pages.build.waitForPcLoaded();
+        });
+        await test.step('Open build summary and take snapshot for further validation', async() => {
+            await pages.build.openSummary();
+            beforeImg = await  (await pages.build.summary.getBuildsummaryDialogElement()).screenshot({path: `screenshots/${testInfo.title}_before.png`});
+        });
+        await test.step('Click I am Finished to get to build quote page', async() => {
+            await pages.build.clickIamFinishedBtn();
+        });
+        await test.step('Get submissionID from url and get load url from database', async() => {
+            const buildId = Common.getBuildIdFromQuoteUrl(pages.page.url());
+            const loadUrlQuery = await SqlHelper.executeQuery(`select LoadUrl from ConfiguredWholegoods where BuildID = '${buildId}'`);
+            loadUrl = loadUrlQuery.recordset[0].LoadUrl;
+        });
+        await test.step('Navigate to load url and verify response is successful', async() => {
+            await pages.navigation.navigateToUrl(loadUrl);
+            await pages.build.waitForPcLoaded();
+        });
+        await test.step('Open build summary and take snapshot after load url loaded', async() => {
+            await pages.build.openSummary();
+            afterImg = await  (await pages.build.summary.getBuildsummaryDialogElement()).screenshot({path: `screenshots/${testInfo.title}_after.png`})
+        });
+        await test.step('Compare build summary snapshots', async() => {
+            expect(comparator(beforeImg, afterImg)).toBeNull();
+        });
+        
+    })
+}
+
+for (const locale of testConfig.domesticLocales.rzr) {
+    test(`Verify rgr build load works as expected for domestic ${locale} @buildLoad`, async( {pages}, testInfo ) => {
+        let beforeImg;
+        let afterImg;
+        let loadUrl;
+    
+        await test.step('Navigate to any rgr build url', async() => {
+            const url = await ApiData.getApiBuildUrl(locale, 'rgr');
+            await pages.navigation.navigateToUrl(url);
+            await pages.build.waitForPcLoaded();
+        });
+        await test.step('Open build summary and take snapshot for further validation', async() => {
+            await pages.build.openSummary();
+            beforeImg = await  (await pages.build.summary.getBuildsummaryDialogElement()).screenshot({path: `screenshots/${testInfo.title}_before.png`});
+        });
+        await test.step('Click I am Finished to get to build quote page', async() => {
+            await pages.build.clickIamFinishedBtn();
+        });
+        await test.step('Get submissionID from url and get load url from database', async() => {
+            const buildId = Common.getBuildIdFromQuoteUrl(pages.page.url());
+            const loadUrlQuery = await SqlHelper.executeQuery(`select LoadUrl from ConfiguredWholegoods where BuildID = '${buildId}'`);
+            loadUrl = loadUrlQuery.recordset[0].LoadUrl;
+        });
+        await test.step('Navigate to load url and verify response is successful', async() => {
+            await pages.navigation.navigateToUrl(loadUrl);
+            await pages.build.waitForPcLoaded();
+        });
+        await test.step('Open build summary and take snapshot after load url loaded', async() => {
+            await pages.build.openSummary();
+            afterImg = await  (await pages.build.summary.getBuildsummaryDialogElement()).screenshot({path: `screenshots/${testInfo.title}_after.png`})
+        });
+        await test.step('Compare build summary snapshots', async() => {
+            expect(comparator(beforeImg, afterImg)).toBeNull();
+        });
+        
+    })
+}
+
+for (const locale of testConfig.domesticLocales.rzr) {
+    test(`Verify atv build load works as expected for domestic ${locale} @buildLoad`, async( {pages}, testInfo ) => {
+        let beforeImg;
+        let afterImg;
+        let loadUrl;
+    
+        await test.step('Navigate to any atv build url', async() => {
+            const url = await ApiData.getApiBuildUrl(locale, 'atv');
+            await pages.navigation.navigateToUrl(url);
+            await pages.build.waitForPcLoaded();
+        });
+        await test.step('Open build summary and take snapshot for further validation', async() => {
+            await pages.build.openSummary();
+            beforeImg = await  (await pages.build.summary.getBuildsummaryDialogElement()).screenshot({path: `screenshots/${testInfo.title}_before.png`});
+        });
+        await test.step('Click I am Finished to get to build quote page', async() => {
+            await pages.build.clickIamFinishedBtn();
+        });
+        await test.step('Get submissionID from url and get load url from database', async() => {
+            const buildId = Common.getBuildIdFromQuoteUrl(pages.page.url());
+            const loadUrlQuery = await SqlHelper.executeQuery(`select LoadUrl from ConfiguredWholegoods where BuildID = '${buildId}'`);
+            loadUrl = loadUrlQuery.recordset[0].LoadUrl;
+        });
+        await test.step('Navigate to load url and verify response is successful', async() => {
+            await pages.navigation.navigateToUrl(loadUrl);
+            await pages.build.waitForPcLoaded();
+        });
+        await test.step('Open build summary and take snapshot after load url loaded', async() => {
+            await pages.build.openSummary();
+            afterImg = await  (await pages.build.summary.getBuildsummaryDialogElement()).screenshot({path: `screenshots/${testInfo.title}_after.png`})
+        });
+        await test.step('Compare build summary snapshots', async() => {
+            expect(comparator(beforeImg, afterImg)).toBeNull();
+        });
+        
+    })
+}
+
+for (const locale of testConfig.domesticLocales.rzr) {
+    test(`Verify grl build load works as expected for domestic ${locale} @buildLoad`, async( {pages}, testInfo ) => {
+        let beforeImg;
+        let afterImg;
+        let loadUrl;
+    
+        await test.step('Navigate to any grl build url', async() => {
+            const url = await ApiData.getApiBuildUrl(locale, 'grl');
             await pages.navigation.navigateToUrl(url);
             await pages.build.waitForPcLoaded();
         });
