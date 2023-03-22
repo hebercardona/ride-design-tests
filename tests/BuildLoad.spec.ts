d3e110e771e2615f6e99bb488249688881944489
hebercardonap <43042222+hebercardonap@users.noreply.github.com>
2023-03-22 15:40:49 -0500

    added test

--- tests/BuildLoad.spec.ts
+++ tests/BuildLoad.spec.ts
@@ -2,6 +2,7 @@ import ApiData from "@framework/ApiData";
 import { test, expect } from "@framework/BaseTest";
 import { Common } from "@framework/Common";
 import SqlHelper from "@framework/SqlClient";
+import { pages } from "@pages/BasePage";
 import { testConfig } from "@testConfig";
 import { getComparator } from 'playwright-core/lib/utils';
 const comparator = getComparator('image/png');
@@ -121,39 +122,42 @@ for (const locale of testConfig.domesticLocales.atv) {
 }
 
 for (const locale of testConfig.domesticLocales.grl) {
-    test(`Verify grl build load works as expected for domestic ${locale} @buildLoad`, async( {pages}, testInfo ) => {
-        let beforeImg;
-        let afterImg;
-        let loadUrl;
-    
+    test(`Verify grl build load works as expected for domestic ${locale} @buildLoadGrl`, async( {pages}, testInfo ) => {
         await test.step('Navigate to any grl build url', async() => {
             const url = await ApiData.getApiBuildUrl(locale, 'grl');
             await pages.navigation.navigateToUrl(url);
             await pages.build.waitForPcLoaded();
         });
-        await test.step('Open build summary and take snapshot for further validation', async() => {
-            await pages.build.openSummary();
-            beforeImg = await  (await pages.build.summary.getBuildsummaryDialogElement()).screenshot({path: `screenshots/${testInfo.title}_before.png`});
-        });
-        await test.step('Click I am Finished to get to build quote page', async() => {
-            await pages.build.clickIamFinishedBtn();
-        });
-        await test.step('Get submissionID from url and get load url from database', async() => {
-            const buildId = Common.getBuildIdFromQuoteUrl(pages.page.url());
-            const loadUrlQuery = await SqlHelper.executeQuery(`select LoadUrl from ConfiguredWholegoods where BuildID = '${buildId}'`);
-            loadUrl = loadUrlQuery.recordset[0].LoadUrl;
-        });
-        await test.step('Navigate to load url and verify response is successful', async() => {
-            await pages.navigation.navigateToUrl(loadUrl);
-            await pages.build.waitForPcLoaded();
-        });
-        await test.step('Open build summary and take snapshot after load url loaded', async() => {
-            await pages.build.openSummary();
-            afterImg = await  (await pages.build.summary.getBuildsummaryDialogElement()).screenshot({path: `screenshots/${testInfo.title}_after.png`})
-        });
-        await test.step('Compare build summary snapshots', async() => {
-            expect(comparator(beforeImg, afterImg)).toBeNull();
-        });
-        
+        await orvBuildLoadTestSteps(pages);
     })
+}
+
+const orvBuildLoadTestSteps = async (pages: pages) => {
+    let beforeImg;
+    let afterImg;
+    let loadUrl;
+
+    await test.step('Open build summary and take snapshot for further validation', async() => {
+        await pages.build.openSummary();
+        beforeImg = await  (await pages.build.summary.getBuildsummaryDialogElement()).screenshot({path: `screenshots/${testInfo.title}_before.png`});
+    });
+    await test.step('Click I am Finished to get to build quote page', async() => {
+        await pages.build.clickIamFinishedBtn();
+    });
+    await test.step('Get submissionID from url and get load url from database', async() => {
+        const buildId = Common.getBuildIdFromQuoteUrl(pages.page.url());
+        const loadUrlQuery = await SqlHelper.executeQuery(`select LoadUrl from ConfiguredWholegoods where BuildID = '${buildId}'`);
+        loadUrl = loadUrlQuery.recordset[0].LoadUrl;
+    });
+    await test.step('Navigate to load url and verify response is successful', async() => {
+        await pages.navigation.navigateToUrl(loadUrl);
+        await pages.build.waitForPcLoaded();
+    });
+    await test.step('Open build summary and take snapshot after load url loaded', async() => {
+        await pages.build.openSummary();
+        afterImg = await  (await pages.build.summary.getBuildsummaryDialogElement()).screenshot({path: `screenshots/${testInfo.title}_after.png`})
+    });
+    await test.step('Compare build summary snapshots', async() => {
+        expect(comparator(beforeImg, afterImg)).toBeNull();
+    });
 }
