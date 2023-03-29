d3fd6d01eb93907c56a78a02aa1acdf53d516ce8
hebercardonap <43042222+hebercardonap@users.noreply.github.com>
2023-03-29 17:53:26 -0500

    updated tests

--- pageFactory/objectRepository/quotePageObjects.ts
+++ pageFactory/objectRepository/quotePageObjects.ts
@@ -9,7 +9,7 @@ export class QuotePageObjects {
     protected static DEALER_NAME = 'div.dealer-selector-element__dropdown-button-dealer-name';
     protected static SEARCH_BTN = 'button.dealer-selector-element__search-button';
     protected static COMMERCIAL_USE_RADIO = `input[type='radio'][value='commercial']`;
-    protected static ORGANIZATION_INPUT_FIELD = `input:below(:text('Organization'))`;
+    protected static ORGANIZATION_INPUT_FIELD = `input:below(:text('Organi'))`;
     protected static MIL_MKT_DROPDOWN = 'div.ValidationRequired select';
     protected static HUR_PURCHASE_DATE = 'div.ValidationRequired select >> nth=1';
     protected static FORM_SUBMISSION_SPINNER = 'div.Form__Submission-Animation';
