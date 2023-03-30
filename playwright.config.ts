dadfad614030dc2df992e6cf7200dd7be64c9533
hebercardonap <43042222+hebercardonap@users.noreply.github.com>
2023-03-30 18:31:06 -0500

    updated config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,6 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices } from '@playwright/test';
-process.env.ENV = `qa`
+process.env.ENV ??= 'qa';
 const ENV = process.env.ENV;
 console.log('ENV is: ' + process.env.ENV);
 
